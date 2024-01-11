a981f3c04274b6c12fe84493784768d554ee28c3
Alireza KafshdarToosi <a.k.toosi@gmail.com>
2024-01-11 11:46:39 +0100

    Use prompt and alert in JS for doing some calculation

---
+++ index.js
@@ -0,0 +1,10 @@
+let name=window.prompt("Hi,Please Enter your name:");
+console.log(`Welcome ${name}`);
+let birthYear=window.prompt(`Please Enter your birth year ${name} ?`);
+console.log(`Your birthyear is ${birthYear} so you are ${2024-birthYear} years old `);
+let Number1=parseInt(window.prompt(`I want to do some calculation so i need two numbers. please enter first number:`));
+let Number2=parseInt(window.prompt(`Please Enter second number`));
+console.log(`${Number1} + ${Number2} = ${Number1+Number2}`);
+console.log(`${Number1} - ${Number2} = ${Number1-Number2}`);
+console.log(`${Number1} * ${Number2} = ${Number1*Number2}`);
+window.alert("Results are ready in the console. please check it.");
